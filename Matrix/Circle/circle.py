2f866aa04b0e70f631c4de9781952677b9508928
nikhilnair90 <113086291+nikhilnair90@users.noreply.github.com>
2022-10-21 12:12:06 +0530

    Add files via upload

--- Matrix/Circle/circle.py
+++ Matrix/Circle/circle.py
@@ -10,7 +10,19 @@ import shlex
 #local imports
 #from line.funcs import *
 #from triangle.funcs import *
-#from conics.funcs import circ_gen
+#from conics.funcs import circ_gen 
+
+def line_dir_pt(m,G,k1,k2):
+   len = 10
+   dim = G.shape[0]
+   x_LC = np.zeros((dim,len))
+   lam_1 = np.linspace(k1,k2,len)
+   for i in range(len):
+     temp1 = G + lam_1[i]*m
+     x_LC[:,i]= temp1.T
+   return x_LC
+
+
 
 #Generating points on a circle
 def circ_gen(O,r):
@@ -24,7 +36,7 @@ def circ_gen(O,r):
 
 
 #Input parameters
-L = np.array(([3,4]))    #point(a,b) on circle 2,3 
+A = np.array(([3,4]))    #point(a,b) on circle 2,3 
 X = np.arange(-5,15,1)   #x points to draw locus
 
 
@@ -33,35 +45,26 @@ u1 = np.array(([0,0]))
 r1 = np.array(([2]))
 
 
-# plotting the locus
-y1=(L[0]**2+L[1]**2+r1**2)
-y2=2*L[0]
-y3=2*L[1]
-Y=(y1-y2*X)/y3    #y points to draw locus
+#plotting the locus
+C=(LA.norm(A)**2+r1**2)/2
+e1=np.array(([1,0]))
+e2=np.array(([0,1]))
 
-plt.plot(X,Y,'r--',label='Locus ');
+g = C/(A@e1)
+G = g*e1
+t = C/(A@e2)
+T = t*e2
 
+plt.axline(G,T, color='red', linestyle='--',label='$Locus$')
 
-#Computation of center and radius of circle2      
-g =3
-t = (y1-y2*g)/y3
-u2 = np.array([g,t])
-#r2 = math.sqrt(LA.norm(u2)**2 - 4)
-r2= LA.norm(u2-L)
-#print(r2)
+#Computation of center and radius of circle2
+u2=G
+r2 = LA.norm(G-A)
 
-#Computation of center and radius of circle3      
-h = 5
-k = (y1-y2*h)/y3 
-u3 = np.array([h,k]) 
-r3=LA.norm(u3-L)
-#r3 = math.sqrt(LA.norm(u3)**2 - 4)                                    
-#print(r3)
 
 #Generating the circle
 x_circ1= circ_gen(u1,r1) 
-x_circ2= circ_gen(u2,r2) 
-x_circ3= circ_gen(u3,r3)
+x_circ2= circ_gen(G,r2) 
 
 # use set_position
 ax = plt.gca()
@@ -72,20 +75,20 @@ ax.spines['bottom'].set_position('zero')
 
 
 #Plotting the circle
-plt.plot(x_circ1[0,:],x_circ1[1,:],label='$Circle1$')
-plt.plot(x_circ2[0,:],x_circ2[1,:],label='$Circle2$')
-plt.plot(x_circ3[0,:],x_circ3[1,:],label='$Circle3$')
+plt.plot(x_circ1[0,:],x_circ1[1,:],color='orange',label='$Circle1$')
+plt.plot(x_circ2[0,:],x_circ2[1,:],color='green',label='$Circle2$')
+
 
 #Labeling the coordinates
-tri_coords = np.vstack((u1,u2,u3,L)).T
+tri_coords = np.vstack((u1,A,u2)).T
 plt.scatter(tri_coords[0,:], tri_coords[1,:])
-vert_labels = ['u1(0,0)','u2(g,t)','u3(h,k)','L(a,b)']
+vert_labels = ['u1(0,0)','A(a,b)','u2']
 for i, txt in enumerate(vert_labels):
     plt.annotate(txt, # this is the text
-                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
-                 textcoords="offset points", # how to position the text
-                 xytext=(0,10), # distance from text to points (x,y)
-                 ha='center') # horizontal alignment can be left, right or center
+            (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
+            textcoords="offset points", # how to position the text
+            xytext=(0,10), # distance from text to points (x,y)
+            ha='center') # horizontal alignment can be left, right or center
 
 
 
