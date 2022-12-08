32f8b784e720edeba3b45cc42d187628a46736db
nikhilnair90 <113086291+nikhilnair90@users.noreply.github.com>
2022-12-08 19:57:35 +0530

    Delete fpga.sh

--- FPGA/fpga.sh
+++
@@ -1,4 +0,0 @@
-ql_symbiflow -compile -src /data/data/com.termux/files/home/fpga-examples/blink -d ql-eos-s3 -P PU64 -v helloworldfpga.v -t helloworldfpga -p pygmy.pcf -dump binary
-
-
-scp helloworldfpga.bin nikhil@192.168.166.241:
